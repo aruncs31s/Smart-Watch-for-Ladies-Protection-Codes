3e83ef0623e440b80c21a23b0aa88669199dbd8e
aruncs31s <aruncs31ss@gmail.com>
2024-12-22 15:55:14 +0530

    SIM800L test

--- src/Config.h
+++ src/Config.h
@@ -1,2 +1,10 @@
 #define BAUD_RATE 9600
 #define MIC_PIN A0
+
+#if defined(ARDUINO)
+#define GSM_TX 3
+#define GSM_RX 2
+#elif defined(ESP8266)
+#define GSM_TX D5
+#define GSM_RX D6
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,54 @@
+/* Source: https://lastminuteengineers.com/sim800l-gsm-module-arduino-tutorial/
+ */
+
 #include "Config.h"
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-void setup() {
-  Serial.begin(BAUD_RATE);
-  pinMode(MIC_PIN, INPUT);
-}
-void loop() {
-  while (1) {
-    Serial.println(analogRead(MIC_PIN));
+// #if defined(ARDUINO)
+SoftwareSerial gsmSerial(GSM_TX, GSM_RX); // 3 -> SIM800L Tx & 2 -> SIM800L Rx
+// #elif defined(ESP8266)
+// SoftwareSerial gsmSerial(D5, D6); // D5 -> SIM800L Tx & D6 -> SIM800L Rx
+// #endif
+
+void updateSerial() {
+  delay(500);
+  while (Serial.available()) {
+    gsmSerial.write(
+        Serial.read()); // Forward what Serial received to Software Serial Port
   }
+  while (gsmSerial.available()) {
+    Serial.write(
+        gsmSerial
+            .read()); // Forward what Software Serial received to Serial Port
+  }
+}
+void setup() {
+  Serial.begin(9600);
+
+  // Start GSM Serial
+  gsmSerial.begin(9600);
+
+  Serial.println("Initializing...");
+  delay(1000);
+
+  gsmSerial.println(
+      "AT"); // Once the handshake test is successful, it will back to OK
+  updateSerial();
+
+  gsmSerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  gsmSerial.println(
+      "AT+CMGS=\"+917902504188\""); // change ZZ with country code and
+                                    // xxxxxxxxxxx with phone number to sms
+  updateSerial();
+  gsmSerial.print(
+      "Last Minute Engineers | lastminuteengineers.com"); // text content
+  updateSerial();
+  gsmSerial.write(26);
+}
+
+void loop() {
+  updateSerial();
+  delay(1000);
 }
