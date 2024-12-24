94fb0379ef75cf7085c432fcf2e522642ff8a905
aruncs31s <aruncs31ss@gmail.com>
2024-12-24 17:35:55 +0530

    edit Tue Dec 24 05:35:55 PM IST 2024

--- src/Config.h
+++ src/Config.h
@@ -1,11 +1,26 @@
 #define BAUD_RATE 9600
 #define MIC_PIN A0
 #define GPS_BAUD_RATE 9600
+// TODO: Uncomment after editing
+// #define ESP8266
+// #if defined(ARDUINO)
+// #define GSM_TX 3
+// #define GSM_RX 2
+// #elif defined(ESP8266)
+#define GPS_TX D5 // This should connect to the TX pin of the GPS module
+#define GPS_RX D6 // This should connect to the RX pin of the GPS module
+// #endif
 
-#if defined(ARDUINO)
-#define GSM_TX 3
-#define GSM_RX 2
-#elif defined(ESP8266)
-#define GSM_TX D5
-#define GSM_RX D6
-#endif
+// Where the push button is connected
+// TODO: Add a decoubling/debouncing capacitor to the button
+#define PUSH_BUTTON D1
+
+typedef struct {
+  bool _gps_status;
+  double _lat;  // latitude
+  double _long; // longitude
+} Location;
+typedef struct {
+  const char *police;
+  const char *parent;
+} Numbers;
--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,64 @@
  * https://randomnerdtutorials.com/esp8266-nodemcu-neo-6m-gps-module-arduino/
  */
 
+#include "Arduino.h"
+#include "Config.h"
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
 
-const int GPS_RX = D5; // This should connect to the TX pin of the GPS module
-const int GPS_TX = D6; // This should connect to the RX pin of the GPS module
-// #define TX D6
-
-// The TinyGPS++ object
+// All Global Object and variable declerations
 TinyGPSPlus gps;
+bool isEmergency;
+Location location = {0, 0, 0};
+Numbers number = {"7902340086", "7902504188"}; // Police and Parent
 
 // Create an instance of Software Serial
 SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
 
-void setup() {
-  // Serial Monitor
-  Serial.begin(115200);
+// This will continue to execute if there is no signal to the GPS module
+void getLocation() {
+  while (!location._gps_status) {
+    if (gpsSerial.available() > 0) {
+      gps.encode(gpsSerial.read());
+      location._gps_status ^= 1;
+    }
+  }
+  location._lat = gps.location.lat();
+  location._long = gps.location.lng();
+  location._gps_status ^= 1;
+}
+bool send_SOS() {
+  /*
+   * @param none
+   * @return -> used to indicate a sucessful SOS
+   */
+  getLocation();
 
+  return 1;
+}
+
+void setup() {
+  Serial.begin(115200);
   // Start Serial 2 with the defined RX and TX pins and a baud rate of 9600
   gpsSerial.begin(GPS_BAUD_RATE);
-  Serial.println("Software Serial started at 9600 baud rate");
+  // Make the ISR quick
+  attachInterrupt(PUSH_BUTTON, send_SOS, FALLING);
 }
 
 void loop() {
-  // This sketch displays information every time a new sentence is correctly
-  // encoded.
-  unsigned long start = millis();
 
+  unsigned long start = millis();
   while (millis() - start < 1000) {
     while (gpsSerial.available() > 0) {
       gps.encode(gpsSerial.read());
     }
-    if (gps.location.isUpdated()) {
-      Serial.print("LAT: ");
-      Serial.println(gps.location.lat(), 6);
-      Serial.print("LONG: ");
-      Serial.println(gps.location.lng(), 6);
-      Serial.print("SPEED (km/h) = ");
-      Serial.println(gps.speed.kmph());
-      Serial.print("ALT (min)= ");
-      Serial.println(gps.altitude.meters());
-      Serial.print("HDOP = ");
-      Serial.println(gps.hdop.value() / 100.0);
-      Serial.print("Satellites = ");
-      Serial.println(gps.satellites.value());
-      Serial.print("Time in UTC: ");
-      Serial.println(String(gps.date.year()) + "/" + String(gps.date.month()) +
-                     "/" + String(gps.date.day()) + "," +
-                     String(gps.time.hour()) + ":" + String(gps.time.minute()) +
-                     ":" + String(gps.time.second()));
-      Serial.println("");
-    }
+
+    location = getLocation();
+    //   if (gps.location.isUpdated()) {
+    //     Serial.print("LAT: ");
+    //     Serial.println(gps.location.lat(), 6);
+    //     Serial.print("LONG: ");
+    //     Serial.println(gps.location.lng(), 6);
+    //   }
   }
 }
